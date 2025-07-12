ecccafb32ba7c07797220eb94dd3d3223fa1c72c
Luis Cassih <luiscassih@gmail.com>
2025-07-12 01:58:19 -0300

    clean up - ready for publish 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# StartWithGun
+# Start with a Gun
+Start each run with a gun or any item you want.
+
+This mod detects at every run level whether you own a gun (or the item you specify in the config). If you don't have one, it will add it automatically.
+
+If you're playing in multiplayer, only the host needs to have this mod enabled.
+
+## Motivation
+The game isn't well balanced for single-player mode, and it becomes frustrating when you don't randomly get any gun in the first shops. While this mod makes the game slightly easier, it makes the experience much more enjoyable. With just a simple handgun, the game doesn't become easy enough to make it boring.
--- StartWithGun.cs
+++ StartWithGun.cs
@@ -10,7 +10,7 @@ using UnityEngine.SceneManagement;
 
 namespace StartWithGun;
 
-[BepInPlugin("StartWithGun.StartWithGun", "StartWithGun", "1.0")]
+[BepInPlugin("UsagiDev.StartWithGun", "StartWithGun", "1.0.0")]
 public class StartWithGun : BaseUnityPlugin
 {
     internal static StartWithGun Instance { get; private set; } = null!;
@@ -18,9 +18,8 @@ public class StartWithGun : BaseUnityPlugin
     private ManualLogSource _logger => base.Logger;
     internal Harmony? Harmony { get; set; }
 
-    public static ConfigEntry<bool> isEnabled;
+    static ConfigEntry<string> gun;
 
-    //public static bool shouldEquip = true;
     private void Awake()
     {
         Instance = this;
@@ -29,7 +28,7 @@ public class StartWithGun : BaseUnityPlugin
         this.gameObject.transform.parent = null;
         this.gameObject.hideFlags = HideFlags.HideAndDontSave;
 
-        isEnabled = this.Config.Bind("StartWithGun", "Enabled", true);
+        gun = this.Config.Bind("Default Item Asset Name", "Gun", "Item Gun Handgun");
         Patch();
         SceneManager.sceneLoaded += OnSceneLoaded;
 
@@ -38,12 +37,10 @@ public class StartWithGun : BaseUnityPlugin
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        //Logger.LogError($"onsceneloaded> scene: {scene.name} mode:{mode} levelcurrent={RunManager.instance.levelCurrent} gameover={RunManager.instance.gameOver} RunisLevel: {SemiFunc.RunIsLevel()}");
-        //Logger.LogError($"purchasedgun:{StatsManager.instance.itemsPurchased["Item Gun Handgun"]}");
-        if (isEnabled.Value && SemiFunc.RunIsLevel() && SemiFunc.IsMasterClientOrSingleplayer())
+        if (SemiFunc.RunIsLevel() && SemiFunc.IsMasterClientOrSingleplayer())
         {
             var purchased = StatsManager.instance.itemsPurchased;
-            const string itemAssetName = "Item Gun Handgun";
+            var itemAssetName = gun.Value;
             if (purchased.TryGetValue(itemAssetName, out var count) && count == 0)
             {
                 purchased[itemAssetName]++;
@@ -63,141 +60,4 @@ public class StartWithGun : BaseUnityPlugin
     {
         Harmony?.UnpatchSelf();
     }
-    
-    private void Update()
-    {
-        // Code that runs every frame goes here
-    }
-
-    /*
-    [HarmonyPatch(typeof(PlayerAvatar), nameof(PlayerAvatar.Start))]
-    class PlayerAvatar_Start
-    {
-        [HarmonyPostfix]
-        static void Postfix(PlayerAvatar __instance)
-        {
-            SemiFunc
-        }
-    }
-    */
-
-    /*[HarmonyPatch(typeof(InventorySpot), "Update")]
-    class InventorySpot_Patch
-    {
-        [HarmonyPostfix]
-        static void Postfix(InventorySpot __instance)
-        {
-            if (shouldEquip && SemiFunc.RunIsLevel() && RunManager.instance.loadLevel == 0)
-            {
-                shouldEquip = false;
-                /*
-                ItemEquippable[] items = Resources.FindObjectsOfTypeAll<ItemEquippable>();
-                foreach (ItemEquippable item in items)
-                {
-                    if (item.name == "Item Gun Handgun")
-                    {
-                        Logger.LogError($"Item found {item.name}, trying to equip");
-                        ItemEquippable gun = Object.Instantiate<ItemEquippable>(item);
-                        gun.name += "_Clone";
-                        Logger.LogError($"Spawned {gun.name}");
-                        gun.RequestEquip(__instance.inventorySpotIndex);
-                        break;
-                        //__instance.EquipItem(item);
-                    }
-                }
-                #1#
-                var items = Resources.FindObjectsOfTypeAll<Item>();
-                foreach (var item in items)
-                {
-                    if (item.name == "Item Gun Handgun")
-                    {
-                        var player = Object.FindObjectOfType<PlayerController>();
-                        if (player != null)
-                        {
-                            // var gun = Object.Instantiate<Item>(item, player.transform.position, Quaternion.identity);
-                            //gun.RequestEquip(__instance.inventorySpotIndex);
-                            // ItemManager.instance.purchasedItems.Add(gun);
-                            StatsManager.instance.itemsPurchased[item.itemAssetName]++;
-                            StatsManager.instance.itemsPurchasedTotal[item.itemAssetName]++;
-                            /*
-                            foreach (var p in ItemManager.instance.purchasedItems)
-                            {
-                                if (p.name == item.name)
-                                {
-                                    p.
-                                }
-                            }
-                            #1#
-                            //Inventory.instance.inventorySpots.ElementAt(0).CurrentItem = gun;
-                            //__instance.CurrentItem = gun;
-                            // Logger.LogError($"inventory: {Inventory.instance.inventorySpots.First()}");
-                            //gun.RequestEquip(__instance.inventorySpotIndex);
-                            Logger.LogError($"Found Player {player.name}, spawning {item.itemAssetName}");
-                            break;
-                        }
-                    }
-                }
-                    
-            }
-        }
-    }*/
-
-    /*
-    [HarmonyPatch(typeof(PlayerController), "Awake")]
-    class PlayerController_Awake_Patch
-    {
-        [HarmonyPostfix]
-        static void Postfix(PlayerController player)
-        {
-            GameObject[] items = Resources.FindObjectsOfTypeAll<GameObject>();
-            foreach (GameObject item in items)
-            {
-                if (item.name == "Item Gun Handgun")
-                {
-                    GameObject gun = Object.Instantiate<GameObject>(item,
-                        player.transform.position + player.transform.forward * 2f, Quaternion.identity);
-                    gun.name += "_Clone";
-                    Logger.LogInfo("Gun spawned OK");
-                }
-            }
-
-        }
-    }
-    [HarmonyPatch(typeof(RunManager))]
-    public static class RunManagerPatch
-    {
-        [HarmonyPatch("ChangeLevel")]
-        [HarmonyPrefix]
-        private static void ChangeLevelPatch(ref RunManager.ChangeLevelType _changeLevelType)
-        {
-            //Logger.LogError($"loadlevel={RunManager.instance.loadLevel}  changeLevelType={_changeLevelType} RunisLevel: {SemiFunc.RunIsLevel()}");
-            if (isEnabled.Value && RunManager.instance.loadLevel == 0 &&
-                SemiFunc.IsMasterClientOrSingleplayer() && _changeLevelType == RunManager.ChangeLevelType.RunLevel)
-            {
-                StatsManager.instance.itemsPurchased["Item Gun Handgun"]++;
-                StatsManager.instance.itemsPurchasedTotal["Item Gun Handgun"]++;
-                Logger.LogError("Added Itemgun");
-            }
-        }
-    }
-    */
-    /*
-    [HarmonyPatch(typeof(RunManager))]
-    public static class RunManagerPatch
-    {
-        [HarmonyPatch("ChangeLevel")]
-        [HarmonyPrefix]
-        private static void ChangeLevelPatch(ref RunManager.ChangeLevelType _changeLevelType)
-        {
-            if (isEnabled.Value && RunManager.instance.loadLevel == 0 &&
-                SemiFunc.IsMasterClientOrSingleplayer() && _changeLevelType == RunManager.ChangeLevelType.RunLevel)
-            {
-                SemiFunc.StatSetRunCurrency(50);
-                _changeLevelType = RunManager.ChangeLevelType.Shop;
-                //ItemEquippable gun = new ItemEquippable().
-            }
-            //Logger.LogInfo($"ASDASDSADS Item in spot1: {StatsManager.instance.playerInventorySpot1}");
-        }
-    }
-*/
 }
